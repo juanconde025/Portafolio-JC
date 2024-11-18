63eec8b5cf2f67ddf7e5bb96e08bae55a95b868e
Juan David Conde Martínez <davidconde1309@gmail.com>
2024-11-18 10:16:36 -0500

    feat: :sparkles: Adicion de modal al dar click en el proyecto
    By @juanconde025

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,32 @@
-let menuVisible = false;
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
-        menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
-        menuVisible = true;
-    }
+const nav = document.getElementById('nav'); 
+const navMenu = nav.querySelector('ul'); 
+const navResponsive = document.querySelector('.nav-responsive'); 
+
+navResponsive.addEventListener('click', () => {
+    navMenu.classList.toggle('active'); 
+});
+
+// Cerrar el menú al seleccionar un enlace
+function seleccionar() {
+    navMenu.classList.remove('active'); 
 }
 
-function seleccionar(){
 
-    document.getElementById("nav").classList = "";
-    menuVisible = false;
-}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('projectModal');
+    const videoFrame = document.getElementById('projectVideo');
+
+    modal.addEventListener('show.bs.modal', (event) => {
+      const trigger = event.relatedTarget;
+      const videoURL = trigger.getAttribute('data-video');
+      const title = trigger.alt;
+
+      modal.querySelector('.modal-title').textContent = title;
+      videoFrame.src = videoURL;
+    });
+
+    modal.addEventListener('hidden.bs.modal', () => {
+      videoFrame.src = '';
+    });
+  });
