47974ee5bfa67a000a40f522252a5a5295381117
Juan David Conde Martínez <davidconde1309@gmail.com>
2024-11-19 14:16:12 -0500

    fix: :bug: Arreglo del modal sin mostrar
    By @juanconde025

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,18 +41,18 @@ document.addEventListener('DOMContentLoaded', () => {
     modal.addEventListener('hidden.bs.modal', () => {
       videoFrame.src = '';
     });
-  });
+});
 
-  const formulario = document.querySelector(".formulario-contacto");
+const formulario = document.querySelector(".formulario-contacto");
 
-  formulario.addEventListener("submit", (e) => {
-      const nombre = document.getElementById("nombre").value.trim();
-      const email = document.getElementById("email").value.trim();
-      const mensaje = document.getElementById("mensaje").value.trim();
+formulario.addEventListener("submit", (e) => {
+const nombre = document.getElementById("nombre").value.trim();
+const email = document.getElementById("email").value.trim();
+const mensaje = document.getElementById("mensaje").value.trim();
   
-      if (!nombre || !email || !mensaje) {
-          e.preventDefault();
-          alert("Por favor, completa todos los campos.");
-      }
-  });
+if (!nombre || !email || !mensaje) {
+    e.preventDefault();
+    alert("Por favor, completa todos los campos.");
+}
+});
   
